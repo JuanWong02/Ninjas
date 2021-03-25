62252b2522df5c55a81c983d7527d8c8ca6eaad5
JuanWong02 <juanwong0211@gmail.com>
2021-03-25 12:05:45 -0700

    Se agrega la clase Player

---
+++ Player.cs
@@ -0,0 +1,57 @@
+using System;
+
+namespace ninjas
+{
+    //Que tiene -> atributos (programacion orientada a objetos)
+    // -colision (*)
+    // - vida / health
+
+
+
+    //Que hace -> metodos (programacion orientada a objetos)
+    // acciones / verbos 
+    // - movimiento -> Moverse() / Move()
+    //ataque -> Atacar() / Attack()
+    class Player 
+    {
+        //Atributos
+        //health
+        public Health health;
+
+        public Movement movement;
+
+        public int attackDamage = 30;
+
+        public Player()
+        {
+            this.health = new Health(100, 100, 100); // 100 son los valores de los parametros de Health()
+
+        }
+
+        //Metodos
+        public void Move()
+        {
+            
+        }
+        /// <summary>
+        /// Attacks a target
+        /// </summary>
+        /// <returns>true if attack was a success</returns>
+        public bool Attack(Enemy target)
+        {
+            if (target == null)
+            {
+                return false;
+            }
+            if (target.health.IsDead())
+            {
+                return false;
+            }
+
+            target.health.TakeDamage(attackDamage);
+
+            return true;
+        }
+    }
+
+}
